a353ef0a253060c3758497c95d7a65fb5eaf48d5
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-10-22 21:59:44 +0530

    Create Numbersquare.py

---
+++ Numbersquare.py
@@ -0,0 +1,11 @@
+number  
+square = 0  
+  
+# Creating an empty list  
+squares = []  
+  
+# Creating a for loop  
+for value in numbers:  
+    square = value ** 2  
+    squares.append(square)  
+print("The list of squares is", squares)  
