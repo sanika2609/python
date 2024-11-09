1d8517bc8dcdece5dd078d01cbaab757d286738b
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-11-09 22:49:19 +0530

    Create positiveNeg.py

---
+++ positiveNeg.py
@@ -0,0 +1,12 @@
+# Python program to demonstrate 
+# nested if with multiple if statements
+
+i = -15; 
+# condition 1
+if i != 0:
+    # condition 2
+    if i > 0:
+        print("Positive")
+    # condition 3
+    if i < 0:
+        print("Negative")
